9ae37588479e772cf45b6e15914888208b297361
haestflod <haestflod@gmail.com>
2016-06-30 23:45:29 +0300

    Loading obj files
    Added obj assets and mtl files.
    Added objFileLoader.
    Loads the Colo_Rug_Fab_LtBrown_001 currently.
    the mtl files have a hardcoded url to C:/ for user tswift which I
    manually removed for now.

--- bjorn/js/app/app.js
+++ bjorn/js/app/app.js
@@ -20,23 +20,22 @@ var createScene = function ()
     camera.setTarget(BABYLON.Vector3.Zero());
 
     // This attaches the camera to the canvas
-    camera.attachControl(canvas, false);
+    camera.attachControl(canvas, false);    
 
     // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Dim the light a small amount
     light.intensity = .5;
-
-    // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
+    
+    var loader = new BABYLON.AssetsManager(scene);
+    var chair = loader.addMeshTask("chair", "", "/bjorn/assets/", "Colo_Rug_Fab_LtBrown_001.obj");
+    
+    loader.load();
+       
     // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
     var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
+    
     // Leave this function
     return scene;
 };  // End of createScene function
@@ -46,7 +45,7 @@ var scene = createScene();
 
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function ()
-{
+{    
     scene.render();
 });
 

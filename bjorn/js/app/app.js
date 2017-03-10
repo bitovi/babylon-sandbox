d1d545acdd74a66d952c6555614a2fd36e40c480
haestflod <haestflod@gmail.com>
2017-03-10 21:34:09 +0200

    clean project for testing features

--- bjorn/js/app/app.js
+++ bjorn/js/app/app.js
@@ -6,12 +6,14 @@
 
     // Load the BABYLON 3D engine
     var engine = new BABYLON.Engine(canvas, true);
+    var ground;
     var item = null;
     var collisions = [];
 
     function createScene () {
         // This creates a basic Babylon Scene object (non-mesh)
         var scene = new BABYLON.Scene(engine);
+        window.scene = scene;
 
         // This creates and positions a free camera (non-mesh)
         var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
@@ -29,23 +31,70 @@
         light.intensity = 0.7;
 
         // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        var ground = BABYLON.Mesh.CreateGround("ground1", 60, 60, 2, scene);
+        ground = BABYLON.Mesh.CreateGround("ground1", 60, 60, 2, scene);
+        ground.material = new BABYLON.StandardMaterial("groundmat", scene);
+        ground.material.specularColor = BABYLON.Color3.Black();
+
+        let itemMaterial = new BABYLON.StandardMaterial( "itemMaterial", scene );
 
         const size = 2;
+        let height = size * 0.5;
 
-        item = BABYLON.MeshBuilder.CreateBox( "item", {
+        let type = localStorage.getItem("type");
+        if ( type ) {
+          type = parseInt( type );
+        }
+
+        if ( type === 1 ) {
+          item = BABYLON.MeshBuilder.CreateSphere("item", {
+            diameter: size
+          }, scene);
+        } else if ( type === 2 ) {
+          item = BABYLON.MeshBuilder.CreateCylinder( "item", {
+            height: size,
+            diameter: size
+          }, scene );
+        } else if ( type === 3 ) {
+          item = BABYLON.MeshBuilder.CreateTorus( "item", {
+            diameter: size
+          }, scene);
+        }
+        else if ( type === 4 ) {
+
+          // Load babylon file
+          BABYLON.SceneLoader.ImportMesh("", "", "data:" + JSON.stringify( babylonfile ), scene, ( meshes ) => {
+            for ( let i = 0; i < meshes.length; ++i ) {
+              meshes[i].isVisible = true;
+              meshes[i].material = itemMaterial;
+            }
+            item = meshes[0];
+          });
+          // For the height!
+          height = 0;
+        }
+        else {
+          item = BABYLON.MeshBuilder.CreateBox( "item", {
             width: size,
             height: size,
             depth: size
-        }, scene );
-        item.material = new BABYLON.StandardMaterial( "itemMaterial", scene );
-        item.material.diffuseColor = BABYLON.Color3.FromHexString("#BADBAD");;
+          }, scene );
+        }
 
-        item.position.y = size * 0.5;
+
+        item.material = itemMaterial;
+        item.material.diffuseColor = BABYLON.Color3.FromHexString( "#BADBAD" );
+        item.material.specularColor = BABYLON.Color3.Black();
+        item.material.alpha = 0.8;
+
+        item.position.y = height;
+        // item.rotation.y = Math.PI * 0.33;
 
         const collisionSize = 1.5;
+        const collisionColor = "#1CE1CE";
         let collisionMaterial = new BABYLON.StandardMaterial( "colMat", scene );
-        collisionMaterial.diffuseColor = BABYLON.Color3.FromHexString( "#D0GD0G" );
+        collisionMaterial.diffuseColor = BABYLON.Color3.FromHexString( collisionColor );
+        collisionMaterial.specularColor = BABYLON.Color3.Black();
+        collisionMaterial.alpha = 0.7;
 
         let collisionOptions = {
           width: collisionSize,
@@ -55,9 +104,9 @@
 
         let collisionPositions = [
           { x: 0, z: 2 },
-          { x: 0, z: -2 },
+          { x: 2.5, z: 2 },
           { x:2, z: 0 },
-          { x:-2, z:0 }
+          { x:2, z:1 }
         ];
 
         for ( let i = 0; i < collisionPositions.length; ++i ) {
@@ -66,11 +115,25 @@
 
           let position = collisionPositions[ i ];
 
-          collisionBox.position.x = size * position.x;
-          collisionBox.position.y = collisionSize * 0.5;
-          collisionBox.position.z = size * position.z;
+          collisionBox.position.x = collisionSize * position.x;
+          collisionBox.position.y = collisionSize * 0.5 + 0.25;
+          collisionBox.position.z = collisionSize * position.z;
+
+          if ( i == 0 ) {
+            collisionBox.rotation.y = Math.PI * 0.33;
+          }
 
           collisions.push( collisionBox );
+        }
+
+        let collisionFurniture = item.clone();
+        let collisionMeshes = [ collisionFurniture, ...collisionFurniture.getChildMeshes() ];
+
+        collisionFurniture.position.copyFromFloats( -3, 0, 0 );
+
+        for ( let i = 0; i < collisionMeshes.length; ++i ) {
+          collisionMeshes[ i ].material = collisionMaterial;
+          collisions.push( collisionMeshes[ i ]);
         }
 
         return scene;
@@ -81,6 +144,8 @@
     }
 
     var scene = createScene();
+
+    window.initMouseEvents( scene, item, ground );
 
     // Register a render loop to repeatedly render the scene
     engine.runRenderLoop(function () {
--- bjorn/js/app/mouseevents.js
+++ bjorn/js/app/mouseevents.js
@@ -2,17 +2,42 @@
  * Created on 9.1.2017.
  */
 "use strict";
-window.initMouseEvents = function ( scene, mesh ) {
+/**
+ *
+ * @param {BABYLON.Scene} scene
+ * @param {BABYLON.Mesh} mesh
+ * @param {BABYLON.Mesh} ground
+ */
+window.initMouseEvents = function ( scene, mesh, ground ) {
 
   let _isMouseDown = false;
+
+  function moveMesh( a_mousePos ) {
+    const x = a_mousePos.x;
+    const y = a_mousePos.y;
+
+    const pickingResult = scene.pick( x, y, ( hitmesh ) => {
+      return hitmesh === ground;
+    } );
+
+    if ( pickingResult.hit ) {
+      mesh.position.x = pickingResult.pickedPoint.x;
+      mesh.position.z = pickingResult.pickedPoint.z;
+    }
+  }
 
   /**
    * @param {MouseEvent} e
    */
   function onmousemove ( e ) {
-    if ( _isMouseDown ) {
-      // move mesh
 
+    if ( _isMouseDown ) {
+      const mousePos = {
+        x: e.clientX,
+        y: e.clientY
+      };
+      // move mesh
+      moveMesh(  mousePos );
     }
   }
 
@@ -20,14 +45,20 @@ window.initMouseEvents = function ( scene, mesh ) {
    * @param {MouseEvent} e
    */
   function onmousedown ( e ) {
-    _isMouseDown = true;
+    if ( e.which === 1 ) {
+      _isMouseDown = true;
+    }
+
   }
 
   /**
    * @param {MouseEvent} e
    */
   function onmouseup ( e ) {
-    _isMouseDown = false;
+    if ( e.which === 1 ) {
+      _isMouseDown = false;
+    }
+
   }
 
   window.addEventListener( "mousemove", onmousemove );
